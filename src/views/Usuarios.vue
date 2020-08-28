8065273cfdce4117543565bc5fd3911258097553
Vinicius Souza <viniciuspodi@gmail.com>
2020-08-27 21:38:30 -0400

    Finalizando a view de Usuários

--- src/views/Usuarios.vue
+++ src/views/Usuarios.vue
@@ -1,61 +1,87 @@
 <template>
   <div>
     <!-- Formulário de Cadastro -->
-    <v-form v-model="valid">
-      <v-container>
-        <v-row>
-          <v-col cols="12" md="4">
-            <v-text-field
-              v-model="usuarioCorrente.nome"
-              :rules="nameRules"
-              :counter="10"
-              label="Nome"
-              required
-            ></v-text-field>
-          </v-col>
+    <v-card max-width="50%" class="mx-auto" v-show="isFormOpen">
+      <v-btn
+        color="red"
+        dark
+        small
+        absolute
+        top
+        right
+        fab
+        @click="isFormOpen = false"
+      >
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+      <v-form v-model="valid">
+        <v-container fluid class="my-6">
+          <v-row justify="center">
+            <v-col cols="12" md="4">
+              <v-text-field
+                v-model="usuarioCorrente.nome"
+                :rules="nomeRules"
+                label="Nome"
+                required
+              ></v-text-field>
+            </v-col>
 
-          <v-col cols="12" md="4">
-            <v-text-field
-              v-model="usuarioCorrente.username"
-              :rules="nameRules"
-              :counter="10"
-              label="Username"
-              required
-            ></v-text-field>
-          </v-col>
-        </v-row>
+            <v-col cols="12" md="4">
+              <v-text-field
+                v-model="usuarioCorrente.username"
+                :rules="usernameRules"
+                label="Username"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
 
-        <v-row>
-          <v-col cols="12" md="4">
-            <v-text-field
-              v-model="usuarioCorrente.senha"
-              :rules="nameRules"
-              :counter="10"
-              label="Senha"
-              type="password"
-              required
-            ></v-text-field>
-          </v-col>
+          <v-row justify="center">
+            <v-col cols="12" md="4">
+              <v-text-field
+                v-model="usuarioCorrente.senha"
+                :rules="senhaRules"
+                label="Senha"
+                type="password"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" md="4">
+              <v-text-field
+                v-model="confirmarSenha"
+                label="Confirmar Senha"
+                :rules="senhaRules"
+                type="password"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
 
-          <v-col cols="12" md="4">
-            <v-text-field
-              v-model="usuarioCorrente.tipo"
-              :rules="nameRules"
-              :counter="10"
-              label="Tipo"
-              required
-            ></v-text-field>
-          </v-col>
-        </v-row>
+          <v-row justify="center">
+            <v-col cols="12" md="4">
+              <v-switch
+                v-model="usuarioCorrente.ativo"
+                :label="`Ativo?`"
+              ></v-switch>
+            </v-col>
+          </v-row>
 
-        <v-btn :disabled="!valid" color="success" class="mr-4" @click="">
-          Salvar
-        </v-btn>
-        <v-btn color="warning" class="mr-4" @click="">
-          Cancelar
-        </v-btn>
-      </v-container>
-    </v-form>
+          <v-row justify="center">
+            <v-btn
+              :disabled="!valid"
+              color="success"
+              class="mr-4"
+              @click="salvar"
+            >
+              Salvar
+            </v-btn>
+            <v-btn color="warning" class="mr-4" @click="cancelar">
+              Cancelar
+            </v-btn>
+          </v-row>
+        </v-container>
+      </v-form>
+    </v-card>
 
     <!-- Listagem de usuários cadastrados -->
     <v-data-table
@@ -69,16 +95,26 @@
           <v-toolbar-title>Usuários Cadastrados</v-toolbar-title>
           <v-divider class="mx-4" inset vertical></v-divider>
           <v-spacer></v-spacer>
-          <v-btn color="success" @click="abrirFormCadastro">Novo Usuário</v-btn>
+          <v-btn color="success" @click="isFormOpen = true" v-show="!isFormOpen"
+            >Novo Usuário</v-btn
+          >
         </v-toolbar>
       </template>
 
       <template v-slot:item.acoes="{ item }">
-        <v-icon small class="mr-2" @click="editItem(item)">
+        <v-icon small class="mr-2" @click="editar(item)" color="primary">
           mdi-pencil
         </v-icon>
-        <v-icon small @click="deleteItem(item)">
-          mdi-delete
+        <v-icon
+          small
+          @click="ativarInativar(item)"
+          v-if="item.ativo"
+          color="green"
+        >
+          mdi-check-bold
+        </v-icon>
+        <v-icon small @click="ativarInativar(item)" v-else color="red">
+          mdi-cancel
         </v-icon>
       </template>
 
@@ -101,118 +137,143 @@ export default {
       { text: "Tipo", value: "role" },
       { text: "Ações", value: "acoes", sortable: false },
     ],
+    geradorId: 5,
+    isFormOpen: false,
+    confirmarSenha: "",
     usuarios: [],
-    editedIndex: -1,
-    editedItem: {
-      name: "",
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
-    },
     usuarioCorrente: {
       nome: "",
       username: "",
       senha: "",
-      role: "",
+      role: "ADMIN",
+      ativo: true,
     },
+    isEdicao: false,
     valid: false,
-    firstname: "",
-    lastname: "",
-    nameRules: [
-      (v) => !!v || "Name is required",
-      (v) => v.length <= 10 || "Name must be less than 10 characters",
+    nomeRules: [
+      (v) => !!v || "Nome é obrigatório",
+      (v) =>
+        (v.length >= 10 && v.length <= 30) ||
+        "Nome precisa ter entre 10 e 30 caracteres",
     ],
-    email: "",
-    emailRules: [
-      (v) => !!v || "E-mail is required",
-      (v) => /.+@.+/.test(v) || "E-mail must be valid",
+    usernameRules: [
+      (v) => !!v || "Username é obrigatório",
+      (v) =>
+        (v.length >= 3 && v.length <= 15) ||
+        "Username precisa ter entre 3 e 15 caracteres",
+    ],
+    senhaRules: [
+      (v) => !!v || "Senha é obrigatório",
+      (v) =>
+        (v.length >= 8 && v.length <= 12) ||
+        "Senha precisa ter entre 8 e 12 caracteres",
     ],
   }),
-
-  computed: {
-    formTitle() {
-      return this.editedIndex === -1 ? "New Item" : "Edit Item";
-    },
-  },
-
-  watch: {
-    dialog(val) {
-      val || this.close();
-    },
-  },
 
   created() {
     this.initialize();
   },
 
   methods: {
-    abrirFormCadastro() {
-      alert("Chamou abrir cadastro");
+    cancelar() {
+      this.usuarioCorrente = {
+        nome: "",
+        username: "",
+        senha: "",
+        role: "ADMIN",
+      };
+      this.confirmarSenha = "";
+      this.isEdicao = false;
     },
 
     initialize() {
       this.usuarios = [
         {
+          id: 0,
           nome: "Jão da Silva",
           username: "jao01",
           senha: "123",
           role: "ADMIN",
+          ativo: true,
         },
         {
+          id: 1,
           nome: "Maria da Silva",
           username: "mariaa",
           senha: "123",
           role: "ADMIN",
+          ativo: true,
         },
         {
+          id: 2,
           nome: "Jão da Silva",
           username: "jao01",
           senha: "123",
           role: "ADMIN",
+          ativo: true,
         },
         {
+          id: 3,
           nome: "Jão da Silva",
           username: "jao01",
           senha: "123",
           role: "ADMIN",
+          ativo: true,
         },
         {
+          id: 4,
           nome: "Jão da Silva",
           username: "jao01",
           senha: "123",
           role: "ADMIN",
+          ativo: true,
         },
       ];
     },
 
-    editItem(item) {
-      this.editedIndex = this.desserts.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialog = true;
-    },
-
-    deleteItem(item) {
-      const index = this.desserts.indexOf(item);
-      confirm("Are you sure you want to delete this item?") &&
-        this.desserts.splice(index, 1);
-    },
-
-    close() {
-      this.dialog = false;
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
-    },
-
-    save() {
-      if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem);
+    salvar() {
+      if (this.usuarioCorrente.senha != this.confirmarSenha) {
+        alert("Senhas não conferem!");
       } else {
-        this.desserts.push(this.editedItem);
+        if (!this.isEdicao) {
+          this.adicionar();
+        } else {
+          this.salvarEdicao();
+        }
+
+        this.isFormOpen = false;
+        this.cancelar();
       }
-      this.close();
+    },
+
+    salvarEdicao() {
+      for (let i = 0; i < this.usuarios.length; i++) {
+        if (this.usuarioCorrente.id == this.usuarios[i].id) {
+          Object.assign(this.usuarios[i], this.usuarioCorrente);
+          break;
+        }
+      }
+    },
+
+    editar(usuario) {
+      Object.assign(this.usuarioCorrente, usuario);
+      this.isEdicao = true;
+      this.isFormOpen = true;
+    },
+
+    adicionar() {
+      this.usuarioCorrente.id = this.geradorId;
+      this.geradorId++;
+      this.usuarios.push(this.usuarioCorrente);
+    },
+
+    ativarInativar(usuario) {
+      for (let i = 0; i < this.usuarios.length; i++) {
+        if (usuario.id == this.usuarios[i].id) {
+          this.usuarios[i].ativo = !this.usuarios[i].ativo;
+          break;
+        }
+      }
     },
   },
 };
