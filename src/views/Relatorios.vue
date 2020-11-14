ff40fc2acfe49ad194133ac0631c9bd6ac008f1b
cleiton <djcleitonduarte@hotmail.com>
2020-11-14 00:32:45 -0400

    ajustando posicionamento e alguns label da tela Relatorios

--- src/views/Relatorios.vue
+++ src/views/Relatorios.vue
@@ -1,150 +1,180 @@
 <template>
+  
   <div>
-    <v-toolbar flat color="white">
+   
+    <v-toolbar flat color="white" >
       <v-toolbar-title>Relatórios</v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-toolbar>
+    </v-toolbar>  
+
+     <!-- inicio msg de validação Select -->    
+      <v-row justify="center">
+      <v-col sm="9">
+        <v-slide-y-transition leave-absolute>
+          <v-alert class="mt-3" type="warning" v-if="validacao != ''">{{
+            validacao
+          }}</v-alert>
+        </v-slide-y-transition>
+      </v-col>
+    </v-row>    
+    <!-- Fim msg de validação Select-->  
+    
     <v-form>
       <v-container fluid>
         <v-card>
-          <v-row>
-            <v-col cols="12" sm="3">
-              <v-text-field
-                label="Total Clientes"
-                value="200"
-                hint="Quantidade total clientes cadastrados."
-                v-model="totalClientes"
-                outlined
-                readonly
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" sm="3">
-              <v-text-field
-                label="Clientes último mês"
-                value="67"
-                hint="Quantidade clientes cadastrados últimos 30 dias."
-                v-model="clientesUltimoMes"
-                outlined
-                readonly
-              ></v-text-field>
-            </v-col>
-            <v-spacer></v-spacer>
-            <v-col cols="12" sm="3">
-              <v-text-field
-                label="Gastos Mês"
-                value="R$2.356,80"
-                hint="Total pedidos no ultimo mês"
-                v-model="gastosMes"
-                outlined
-                readonly
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="12" sm="3">
-              <v-text-field
-                label="Total Empresas"
-                value="20"
-                hint="Quantidade total de empresas cadastradas"
-                v-model="totalEmpresas"
-                outlined
-                readonly
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" sm="3">
-              <v-text-field
-                label="Empresas Ultimo Mês"
-                value="5"
-                hint="Quantidade empresas cadastradas últimos 30 dias"
-                v-model="EmpresasUltimoMes"
-                outlined
-                readonly
-              ></v-text-field>
-            </v-col>
-            <v-spacer></v-spacer>
-            <v-col cols="12" sm="3">
-              <v-text-field
-                label="Receita Mês"
-                value="R$2.356,80"
-                hint="Receita gerada pelas empresas no mês"
-                v-model="receitaMes"
-                outlined
-                readonly
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-spacer></v-spacer>
-            <v-col cols="12" sm="3">
-              <v-text-field
-                label="Receita Anual"
-                value="R$10.000,00"
-                hint="Receita total no útimo ano"
-                v-model="receitaAnual"
-                outlined
-                readonly
-              ></v-text-field>
-            </v-col>
-          </v-row>
-
-          <v-row>
-            <v-col class="d-flex" cols="12" sm="6">
-              <v-select
-                :items="empresa"
-                item-text="nome"
-                item-value="nome"
-                label="Empresa"
-                v-model="selectEmpresa"
-                dense
-                solo
-              ></v-select>
-              <v-spacer></v-spacer>
-              <v-btn color="green" @click="buscarEmpresa">Buscar</v-btn>
-            </v-col>
-          </v-row>
-          <v-row justify="center">
-            <v-col sm="9">
-              <v-slide-y-transition leave-absolute>
-                <v-alert class="mt-3" type="warning" v-if="validacao != ''">{{
-                  validacao
-                }}</v-alert>
-              </v-slide-y-transition>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="12" sm="3">
-              <v-text-field
-                label="Total Transações"
-                value="empresa.totalTransacoes"
-                item-text="empresa.totalTransacoes"
-                hint="Todas transações da empresa selecionada"
-                v-model="totalTransacoes"
-                outlined
-                readonly
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" sm="3">
-              <v-text-field
-                label="Total Lanaskina"
-                value="empresa.totalLanaskina"
-                item-text="empresa.totalLaskina"
-                hint="Total mensalidade Lanaskina"
-                v-model="totalLanaskina"
-                outlined
-                readonly
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" sm="3">
-              <v-text-field
-                label="Comissão Lanaskina"
-                value="empresa.comissao"
-                hint="Comissão vendas Lanaskina"
-                v-model="comissao"
-                outlined
-                readonly
-              ></v-text-field>
-            </v-col>
-          </v-row>
+          <v-card>
+            <v-card-title class="headline "
+              >Informações por Empresa
+            </v-card-title>
+            <v-card-text>
+              <v-row no-gutters justify="space-between">
+                <v-col class="d-flex mt-3" cols="12" sm="5">
+                  <v-select
+                    :items="empresa"
+                    item-text="nome"
+                    item-value="nome"
+                    label="Empresa"
+                    v-model="selectEmpresa"
+                    solo
+                  ></v-select>
+                  <v-btn class="mx-3" large color="green" @click="buscarEmpresa"
+                    >Buscar</v-btn
+                  >
+                </v-col>
+                <v-col class="d-flex" cols="12" sm="6">
+                  <v-row>
+                    <v-col>
+                      <v-text-field
+                        label="Total Transação "
+                        value="empresa.totalTransacoes"
+                        item-text="empresa.totalTransacoes"
+                        hint="Quantidade de transações da empresa no APP"
+                        v-model="totalTransacoes"
+                        outlined
+                        readonly
+                      ></v-text-field>
+                    </v-col>
+                    <v-col>
+                      <v-text-field
+                        label="Total movimentado (R$)"
+                        value="empresa.totalLanaskina"
+                        item-text="empresa.totalLaskina"
+                        hint="Total em reais movimentado no APP"
+                        v-model="totalLanaskina"
+                        outlined
+                        readonly
+                      ></v-text-field>
+                    </v-col>
+                    <v-col>
+                      <v-text-field
+                        label="Comissão Lanaskina (R$)"
+                        value="empresa.comissao"
+                        hint="Comissão por vendas para o APP"
+                        v-model="comissao"
+                        outlined
+                        readonly
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+          <hr />
+          <v-card>
+            <v-card-title class="headline ">Informações do Mês</v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col cols="12" sm="3">
+                  <v-text-field
+                    label="Novos Clientes"
+                    value="67"
+                    hint="Quantidade clientes cadastrados no mês"
+                    v-model="clientesUltimoMes"
+                    outlined
+                    readonly
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="3">
+                  <v-text-field
+                    label="Novas Empresas"
+                    value="5"
+                    hint="Quantidade empresas cadastradas no mês"
+                    v-model="EmpresasUltimoMes"
+                    outlined
+                    readonly
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="3">
+                  <v-text-field
+                    label="Movimentação do mês (R$)"
+                    value="R$2.356,80"
+                    hint="Total pedidos no ultimo mês"
+                    v-model="gastosMes"
+                    outlined
+                    readonly
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="3">
+                  <v-text-field
+                    label="Receita do mês"
+                    value="R$2.356,80"
+                    hint="Receita gerada pelas empresas no mês"
+                    v-model="receitaMes"
+                    outlined
+                    readonly
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+          <hr />
+          <v-card>
+            <v-card-title class="headline ">Totalizadores </v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col cols="12" sm="3">
+                  <v-text-field
+                    label="Total Clientes"
+                    value="200"
+                    hint="Quantidade total clientes cadastrados."
+                    v-model="totalClientes"
+                    outlined
+                    readonly
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="3">
+                  <v-text-field
+                    label="Total Empresas"
+                    value="20"
+                    hint="Quantidade total de empresas cadastradas"
+                    v-model="totalEmpresas"
+                    outlined
+                    readonly
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="3">
+                  <v-text-field
+                    label="Total Movimentação "
+                    value="R$10.000,00"
+                    hint="Receita total até o mês"
+                    v-model="receitaAnual"
+                    outlined
+                    readonly
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="3">
+                  <v-text-field
+                    label="Total Receita  "
+                    value="R$10.000,00"
+                    hint="Receita total até o mês"
+                    v-model="receitaAnual"
+                    outlined
+                    readonly
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
         </v-card>
       </v-container>
     </v-form>
@@ -166,7 +196,7 @@ export default {
     EmpresasUltimoMes: "",
     totalEmpresas: "",
     totalClientes: "",
-    clientesUltimoMes: "",
+    clientesUltimoMes: ""
   }),
   created() {
     this.initialize();
@@ -181,7 +211,7 @@ export default {
           totalLanaskina: 210,
           comissao: 220,
           clientes: 43,
-          clientesNovosUltimoMes: 5,
+          clientesNovosUltimoMes: 5
         },
 
         {
@@ -191,7 +221,7 @@ export default {
           totalLanaskina: 210,
           comissao: 120,
           clientes: 19,
-          clientesNovosUltimoMes: 2,
+          clientesNovosUltimoMes: 2
         },
 
         {
@@ -201,7 +231,7 @@ export default {
           totalLanaskina: 210,
           comissao: 320,
           clientes: 19,
-          clientesNovosUltimoMes: 2,
+          clientesNovosUltimoMes: 2
         },
 
         {
@@ -211,7 +241,7 @@ export default {
           totalLanaskina: 210,
           comissao: 420,
           clientes: 19,
-          clientesNovosUltimoMes: 2,
+          clientesNovosUltimoMes: 2
         },
 
         {
@@ -221,7 +251,7 @@ export default {
           totalLanaskina: 210,
           comissao: 120,
           clientes: 19,
-          clientesNovosUltimoMes: 2,
+          clientesNovosUltimoMes: 2
         },
 
         {
@@ -231,48 +261,42 @@ export default {
           totalLanaskina: 210,
           comissao: 220,
           clientes: 19,
-          clientesNovosUltimoMes: 2,
-        },
+          clientesNovosUltimoMes: 2
+        }
       ];
 
       this.totalEmpresas = this.empresa.length;
       this.EmpresasUltimoMes = 3;
 
       for (let i = 0; i < this.empresa.length; i++) {
-        this.receitaAnual =  parseFloat( this.receitaAnual +
-          this.empresa[i].totalLanaskina + this.empresa[i].comissao
+        this.receitaAnual = parseFloat(
+          this.receitaAnual +
+            this.empresa[i].totalLanaskina +
+            this.empresa[i].comissao
         );
-        this.receitaMes = parseFloat( this.receitaMes +
-          this.empresa[i].totalLanaskina + this.empresa[i].comissao
+        this.receitaMes = parseFloat(
+          this.receitaMes +
+            this.empresa[i].totalLanaskina +
+            this.empresa[i].comissao
         );
-        this.gastosMes = parseFloat(this.gastosMes + this.empresa[i].totalTransacoes);
-        this.totalClientes = parseFloat(this.totalClientes + this.empresa[i].clientes);
-        this.clientesUltimoMes = parseFloat(this.clientesUltimoMes +
-          this.empresa[i].clientesNovosUltimoMes
+        this.gastosMes = parseFloat(
+          this.gastosMes + this.empresa[i].totalTransacoes
         );
-
-        console.log(this.receitaAnual);
-        console.log(this.receitaMes);
-        console.log(this.gastosMes);
-        console.log(this.totalClientes);
-        console.log(this.clientesUltimoMes);
+        this.totalClientes = parseFloat(
+          this.totalClientes + this.empresa[i].clientes
+        );
+        this.clientesUltimoMes = parseFloat(
+          this.clientesUltimoMes + this.empresa[i].clientesNovosUltimoMes
+        );
       }
     },
     buscarEmpresa() {
-      if (this.selectEmpresa == "") {
-        this.validacao = "Selecione uma empresa";
+      if (this.selectEmpresa == "") {         
+       return  this.validacao = "Selecione uma empresa";
       }
-      for (let i = 0; i < this.empresa.length; i++) {
-        if (this.empresa[i].nome == this.selectEmpresa) {
-          this.totalTransacoes = this.empresa[i].totalTransacoes;
-          this.totalLanaskina = this.empresa[i].totalLanaskina;
-          this.comissao = this.empresa[i].comissao;
-        }
-      }
-
       this.validacao = "";
-    },
-  },
+    }
+  }
 };
 </script>
 
